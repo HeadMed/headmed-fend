7535bd979dd89961552769bcdde4552da6b3fc09
Arthur <artu.donascimento@gmail.com>
2025-05-25 22:33:59 -0300

    feat: creating HeadMed home page nav bar

---
+++ src/app/(home)/(template)/navBar.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import React from 'react'
+import logo from '../../../assets/Logo.svg'
+import { Button } from '@/components/ui/button'
+export const NavBar = () => {
+  return (
+    <div className="w-full h-16 bg-white flex items-center shadow-xl justify-between px-4">
+      <Image src={logo} alt="logo" width={120} />
+      <div className='flex-1'>
+        <ul className="flex items-center justify-center gap-16 [&>li]:hover:cursor-pointer [&>li]:hover:text-brand-200 [&>li]:text-lg [&>li]:font-semibold">
+          <li>Recursos</li>
+          <li>Como Funciona</li>
+          <li>Preços</li>
+          <li>Equipe</li>
+        </ul>
+      </div>
+      <div className='flex items-center justify-center gap-4'>
+        <Button variant={"ghost"} className="px-4 hover:cursor-pointer">
+          Login
+        </Button>
+        <Button className="bg-brand-200 hover:bg-brand-200/80 hover:cursor-pointer px-4">
+          Registrar
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,35 +1,12 @@
-import Image from "next/image";
-import React from "react";
-import Logo from "../../assets/Logo.svg";
-import { FirstSection } from "./(sections)/firstSection";
-import { TeamSection } from "./(sections)/teamSection";
+import React from 'react'
+import { NavBar } from './(template)/navBar'
+
 const page = () => {
   return (
-    <div className="h-dvh w-dvw flex flex-col ">
-      <nav className="w-full h-16 bg-zinc-100 shadow-lg  flex items-center justify-between p-2 mr-1">
-        <Image
-          className="pl-3 mb-2"
-          src={Logo}
-          alt="HeadMed logo"
-          width={118.5}
-          height={35}
-        />
-        <ul
-          className="flex items-center justify-center gap-4 pr-4
-          [&>li]:text-lg [&>li]:font-bold [&>li]:text-zinc-800 [&>li]:hover:text-brand-200 [&>li]:hover:underline [&>li]:cursor-pointer"
-        >
-          <li>sobre</li>
-          <li>pricing</li>
-          <li>FAQ</li>
-        </ul>
-      </nav>
-
-      <main className="h-[calc(100vh-6rem)] overflow-x-hidden">
-        <FirstSection />
-        <TeamSection />
-      </main>
+    <div>
+      <NavBar/>
     </div>
-  );
-};
+  )
+}
 
-export default page;
+export default page
