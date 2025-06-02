f3346008069eba953245910fa2d9a0b4bf50bcdc
PedroLung <pguedeslsilva@gmail.com>
2025-06-02 00:56:22 -0300

    feat: patient-adjustment and fetch adjustment

--- src/app/scribe-ai/(pages)/patientsPage.tsx
+++ src/app/scribe-ai/(pages)/patientsPage.tsx
@@ -2,14 +2,15 @@
 import React from 'react'
 import { PatientCard } from '../(components)/patientCard';
 import { usePatients } from '@/hooks/usePatients'; 
+import { calculateAge } from '@/utils/utilsData';
 export const PatientsPage = () => {
 const { patients, loading, error } = usePatients();
 
   return (
-    <div className="h-full flex-1 flex items-center justify-center ">
-      <div  className="w-3/4 grid grid-cols-3 grid-rows-2 md:grid-cols-2 gap-4">
+    <div className="h-full flex-1 flex items-start justify-center">
+      <div  className="flex-col w-full px-5 py-10 space-y-3">
         {patients.map((p) => (
-          <PatientCard key={p.id} name={p.nome} id={p.id} />
+          <PatientCard key={p.id} name={p.nome} id={p.id} cpf={p.cpf_display} idade={calculateAge(p.data_nascimento)} phone='(99) 9999-9999' qnt={p.prontuarios?.length || 0}  />
         ))}
       </div>
     </div>
